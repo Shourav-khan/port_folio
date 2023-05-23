ccf87fc6b6287ce081649514a15da6c3aeb561d9
Shourav Khan <102287391+Shourav-khan@users.noreply.github.com>
2023-05-24 00:12:19 +0600

    test change

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -3,7 +3,7 @@
 
 <main>
     <div class="container-fluid px-4">
-        <h1 class="mt-4">Shourav_Dash</h1>
+        <h1 class="mt-4">Shourav_Khan</h1>
         <ol class="breadcrumb mb-4">
             <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
             <li class="breadcrumb-item active">Shourav_Dash</li>
